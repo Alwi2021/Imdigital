1470d95d221538f73594b60bd37371ef301a7245
every soul has only one god the mobile religion <148715519+Alwi2021@users.noreply.github.com>
2026-04-06 02:16:29 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,7 @@ import streamlit as st
 from google import genai
 
 # Setup Klien dengan API Key dari Secrets
-client = genai.Client(api_key=st.secrets[api_key= "AIzaSyCH2yiCqUZoceiJvNo1BycDAfKZPuNGKtw
-"])
+client = genai.Client(api_key=st.secrets[api_key= AIzaSyCH2yiCqUZoceiJvNo1BycDAfKZPuNGKtw])
 
 st.title("🐉 Imdigital - Gemini 3 Core")
 
